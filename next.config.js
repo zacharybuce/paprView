e8f63665345b41aba652b48bd8d76aefdfdf5472
Zachary Buce <55308132+zacharybuce@users.noreply.github.com>
2021-10-10 17:25:35 +0100

    Delete next.config.js
    Accidentally exposed URI

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://Admin:CuZF2SckuerzMijI@paprview.tt3jx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  },
-};
