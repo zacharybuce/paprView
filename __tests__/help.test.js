73e0e64e617472e6c91c366241b028a504ee62b4
zacharybuce <zachary.buce2000@gmail.com>
2022-01-27 16:43:53 +0000

    progress with unit tests

---
+++ __tests__/help.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Help from "../pages/help.js";
+jest.mock("next-auth/react");
+
+describe("help page", () => {
+  test("text appears", async () => {
+    render(<Help />);
+    // // Act
+    // // Assert
+    expect(screen.getByText(/About paprView/)).toBeInTheDocument();
+  });
+});
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import MenuIcon from "@mui/icons-material/Menu";
 import {
   Link,
   Grid,
